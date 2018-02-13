f4bf2938e6e1494f29e523481f2cb86c5fcb9e45
Danielapariona <daniela.parionac@gmail.com>
2018-02-13 18:30:44 -0500

    Show 5 results

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const searchField = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
 let searchedFortext;
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
   e.preventDefault();
   responseContainer.innerHTML = '';
   searchedFortext = searchField.value;
@@ -25,15 +25,20 @@ function handleError() {
 function addNews() {
   //console.log(this.responseText);
   const data = JSON.parse(this.responseText);
-  // const response = data.response;
+  const response = data.response;
   // console.log(response);
-  const article = data.response.docs[0];
-  const title = article.headline.main;
-  const snippet = article.snippet;
 
-  let li = document.createElement('li');
-  li.className = 'articleClass';
-  li.innerHTML = snippet;
-  
-  responseContainer.appendChild(li);
+  for (let i = 0; i < 5; i++) {
+    const article = data.response.docs[i];
+    const title = article.headline.main;
+    const snippet = article.snippet;
+
+    // console.log(response.docs[i].headline);
+    
+    let li = document.createElement('li');
+    li.className = 'articleClass';
+    responseContainer.appendChild(li);
+    li.innerHTML = snippet;
+    responseContainer.appendChild(li);
+  }
 }
